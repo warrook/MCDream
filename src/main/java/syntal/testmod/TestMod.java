b62d5186d805efa3ea13b44797ec136e00cc1ed9
warrook <rook.kingsley@gmail.com>
2018-12-10 17:08:53 -0800

    Initial

---
+++ src/main/java/syntal/testmod/TestMod.java
@@ -0,0 +1,13 @@
+package syntal.testmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = TestMod.MODID, name = TestMod.MODNAME, version = TestMod.MODVERSION)
+public class TestMod
+{
+    public static final String MODID = "testmod";
+    public static final String MODNAME = "Test Mod";
+    public static final String MODVERSION = "0.0.1";
+
+
+}
